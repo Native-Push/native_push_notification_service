2220d1d0e7312c7a1ea056655b8f04174b1a230d
Sven Op de Hipt <eisberg-zirkel-0c@icloud.com>
2024-05-19 23:46:17 +0200

    inital commit

---
+++ README.md
@@ -0,0 +1,54 @@
+# Native Push Notification Service
+
+## Overview
+
+`NativePushNotificationService` is a notification service extension for iOS, designed to handle and modify push notifications before they are delivered to the user. This extension allows for modifications such as adding images to notifications based on data included in the push payload.
+
+## Features
+
+- **Modifies Notification Content**: Customize notification content including title, body, and attachments.
+- **Image Attachments**: Fetches and adds images to notifications based on URLs provided in the push payload.
+- **Graceful Handling of Time Expiry**: Ensures best attempt content is delivered if the service extension is about to be terminated by the system.
+
+## Installation
+
+To use `NativePushNotificationService` in your project, follow these steps:
+
+### Swift Package Manager
+
+1. In Xcode, open your project and navigate to **File > Add Package Dependencies**.
+2. Enter the URL of the `NativePushNotificationService` repository.
+3. Choose the version rule and proceed to add the package to your project.
+
+### Setting Up Notification Service Extension
+
+1. Create a new Notification Service Extension target in your Xcode project.
+2. Replace the content of `NotificationService.swift` with the provided `NotificationService` code.
+3. Add `Background Modes` with `Remote Notification` the capabilites of your main app.
+
+## Example Notification Service
+
+Here is an example of how to use `NativePushNotificationService` in your notification service extension:
+
+```swift
+import NativePushNotificationService
+
+final class NotificationService: NativePushNotificationService {}
+```
+
+## Related projects
+
+- [Native Push](https://github.com/Native-Push/native_push): The main Flutter library
+- [Native Push Client](https://github.com/Native-Push/native_push_client): The Flutter library to be used with `Native Push Server`
+- [Native Push Server](https://github.com/Native-Push/native_push_server): The server which can be used a a microservices and a
+  library which can be used for an own server.
+- [Native Push Vapid](https://github.com/Native-Push/native_push_vapid): The library which should be used to generate Vapid Keys if
+  web is a supported platform.
+
+## Contributing
+
+Contributions are welcome! Feel free to open issues or submit pull requests.
+
+## License
+
+This project is licensed under the BSD-3 License. See the [LICENSE](LICENSE) file for details.
---
+++ Sources/NativePushNotificationService/NativePushNotificationService.swift
@@ -0,0 +1,81 @@
+//
+//  NativePushNotificationService.swift
+//  NativePushNotificationService
+//
+//  Created by Sven Op de Hipt on 06.05.24.
+//
+
+import UserNotifications
+
+/// A notification service extension to handle modifications to push notifications before they are delivered to the user.
+open class NativePushNotificationService: UNNotificationServiceExtension {
+    private var contentHandler: ((UNNotificationContent) -> Void)?
+    private var bestAttemptContent: UNMutableNotificationContent?
+
+    /// Called when a notification is received. This method can be used to modify the notification content.
+    /// - Parameters:
+    ///   - request: The notification request.
+    ///   - contentHandler: The content handler to execute with the modified notification content.
+    open override func didReceive(_ request: UNNotificationRequest, withContentHandler contentHandler: @escaping (UNNotificationContent) -> Void) {
+        self.contentHandler = contentHandler
+        bestAttemptContent = (request.content.mutableCopy() as? UNMutableNotificationContent)
+        
+        guard let bestAttemptContent else {
+            return
+        }
+        bestAttemptContent.title = request.content.title
+        bestAttemptContent.body = request.content.body
+        guard let urlString = bestAttemptContent.userInfo["native_push_image"] as? String else {
+            contentHandler(bestAttemptContent)
+            return
+        }
+        guard let imageUrl = URL(string: urlString) else {
+            contentHandler(bestAttemptContent)
+            return
+        }
+        guard let imageData = NSData(contentsOf: imageUrl) else {
+            contentHandler(bestAttemptContent)
+            return
+        }
+        
+        guard let attachment = UNNotificationAttachment.saveImageToDisk(fileIdentifier: "notification_\(request.identifier).jpg", data: imageData) else {
+            contentHandler(bestAttemptContent)
+            return
+        }
+        bestAttemptContent.attachments = [attachment]
+        contentHandler(bestAttemptContent)
+    }
+    
+    /// Called just before the service extension is terminated by the system.
+    /// This method is used to deliver the best attempt at modified content if the extension's time has expired.
+    open override func serviceExtensionTimeWillExpire() {
+        if let contentHandler = contentHandler, let bestAttemptContent = bestAttemptContent {
+            contentHandler(bestAttemptContent)
+        }
+    }
+}
+
+private extension UNNotificationAttachment {
+    /// Saves an image to disk and returns a notification attachment.
+    /// - Parameters:
+    ///   - fileIdentifier: A unique identifier for the file.
+    ///   - data: The image data to be saved.
+    ///   - options: Options for the attachment.
+    /// - Returns: A UNNotificationAttachment if the file is successfully saved, otherwise nil.
+    static func saveImageToDisk(fileIdentifier: String, data: NSData, options: [NSObject : AnyObject]? = nil) -> UNNotificationAttachment? {
+        let fileManager = FileManager.default
+        let folderName = ProcessInfo.processInfo.globallyUniqueString
+        guard let folderURL = NSURL(fileURLWithPath: NSTemporaryDirectory()).appendingPathComponent(folderName, isDirectory: true) else {
+            return nil
+        }
+        
+        do {
+            try fileManager.createDirectory(at: folderURL, withIntermediateDirectories: true)
+            let fileURL = folderURL.appendingPathComponent(fileIdentifier)
+            try data.write(to: fileURL, options: [])
+            return try UNNotificationAttachment(identifier: fileIdentifier, url: fileURL, options: options)
+        } catch {
+            return nil
+        }
+    }
+}
